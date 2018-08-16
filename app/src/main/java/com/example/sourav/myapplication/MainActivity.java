84df585fc8dcf7fef0dd2313d11424abe931c089
vimmiso <vimmiso0382@gmail.com>
2018-08-16 20:54:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sourav/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sourav.myapplication;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Object my_profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
